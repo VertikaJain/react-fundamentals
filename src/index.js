3912a05b8d2ba884bbfb533c93cb8c99ba5f37fb
Vertika Jain <vertikajain96@gmail.com>
2020-12-30 16:29:55 +0530

    Nested Components

--- src/index.js
+++ src/index.js
@@ -2,7 +2,26 @@ import React from "react";
 import ReactDom from "react-dom";
 
 // Component names need to be capitalized in React, or they won't work.
-function Greeting() {
-  return <h4>hello world</h4>;
+function BookList() {
+  return (
+    <div>
+      <Book />
+      <Place />
+    </div>
+  );
 }
-ReactDom.render(<Greeting />, document.getElementById("root"));
+
+// Nested Components
+const Book = () => <h1>harry potter</h1>;
+const Place = () => <p>crossword</p>;
+
+/* // Using createElement for each HTML component-> Not recommended
+const Greeting = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "hello people")
+  );
+}; */
+
+ReactDom.render(<BookList />, document.getElementById("root"));
