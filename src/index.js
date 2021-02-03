a1b5a244bced92c9687c89bfa6bb887c58964452
Vertika Jain <vertikajain96@gmail.com>
2021-02-03 17:54:28 +0530

    Simple and Complex events using onClick

--- src/index.js
+++ src/index.js
@@ -34,15 +34,25 @@ function BookList() {
   );
 }
 
+const clickHandler = () => {
+  alert("hey there!")
+}
+const clickComplexHandler = (message) => {
+  console.log(message);
+}
+
 // Nested Components
 const Book = ({ author, img, title }) => {
   // let { author, img, title } = props
   return (
     <article className="book">
       <img src={img} alt="" />
-      <h1>{title}</h1>
+      <h1 onClick={() => { console.log("title is : ", title) }}>{title}</h1> {/* Inline function example */}
       {/* Inline CSS overwrites the corresponding properties defined in the imported CSS file. */}
       <h4 style={{ color: 'grey', fontSize: '0.75rem', marginTop: '0.45rem' }}>{author}</h4>
+      {/* Event Handling */}
+      <button type="button" onClick={clickHandler}>Click Me</button> {/* Reference example */}
+      <button type="button" onClick={() => clickComplexHandler(author)}>Complex Click</button> {/* Complex example -> when passing an arguement, use arrow function */}
     </article>
   )
 };
